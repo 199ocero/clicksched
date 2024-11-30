823192c39e7cf4b4e86e104d20434231b3ab2cb5
199ocero <199ocero@gmail.com>
2024-11-30 13:34:48 +0800

    added components and validation

---
+++ resources/js/shadcn/ui/form/FormLabel.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { cn } from '@/lib/utils';
+import { Label } from '@/shadcn/ui/label';
+import type { LabelProps } from 'radix-vue';
+import type { HTMLAttributes } from 'vue';
+import { useFormField } from './useFormField';
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>();
+
+const { error, formItemId } = useFormField();
+</script>
+
+<template>
+    <Label
+        :class="cn(error && 'text-red-600 dark:text-red-400', props.class)"
+        :for="formItemId"
+    >
+        <slot />
+    </Label>
+</template>
