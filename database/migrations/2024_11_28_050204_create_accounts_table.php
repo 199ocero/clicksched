72cf5885ab892d63e4af3fb719b02789e8ae366f
199ocero <199ocero@gmail.com>
2024-12-03 10:16:09 +0800

    fix accounts

--- database/migrations/2024_11_28_050204_create_accounts_table.php
+++ database/migrations/2024_11_28_050204_create_accounts_table.php
@@ -14,15 +14,19 @@ return new class extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->unsignedBigInteger('sociable_id');
-            $table->string('sociable_type');
-            $table->string('name');
-            $table->string('handle');
-            $table->string('profile_image_url');
-            $table->string('access_token');
-            $table->string('refresh_token')->nullable();
-            $table->timestamp('token_expires_at')->nullable();
+            $table->unsignedBigInteger('sociable_id')->nullable()->default(null);
+            $table->string('sociable_type')->nullable()->default(null);
+            $table->string('platform');
+            $table->string('name')->nullable()->default(null);
+            $table->string('handle')->nullable()->default(null);
+            $table->string('email')->nullable()->default(null);
+            $table->string('profile_image_url')->nullable()->default(null);
+            $table->string('access_token')->nullable()->default(null);
+            $table->string('refresh_token')->nullable()->default(null);
+            $table->timestamp('token_expires_at')->nullable()->default(null);
             $table->timestamps();
+
+            $table->unique(['user_id', 'sociable_id', 'sociable_type', 'handle']);
         });
     }
 
